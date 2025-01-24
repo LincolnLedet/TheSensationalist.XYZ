e7ef8077eaf12fcbb5d003bc42b004d2431f7c97
LincolnLedet <lincolnledet@gmail.com>
2025-01-24 18:21:57 +0000

    api fix

--- sensationalistbackend/index.js
+++ sensationalistbackend/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 app.use(express.json());
 
 // 4. Serve static files from the 'uploads' directory
-app.use('/uploads', (req, res, next) => {
+app.use('/api/uploads', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Frontend origin
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
