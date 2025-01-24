1c3c9e2a860b3b75bdb3c69e6707450f199bbbef
Lincoln Ledet <lincolnledet@gmail.com>
2025-01-24 13:43:55 -0500

    changes?

--- sensationalistbackend/index.js
+++ sensationalistbackend/index.js
@@ -26,6 +26,7 @@ app.use(express.json());
 // 4. Serve static files from the 'uploads' directory
 app.use('/uploads', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Frontend origin
+  res.header('Access-Control-Allow-Origin', 'http://http://192.168.0.117:3000/'); // Frontend origin
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
