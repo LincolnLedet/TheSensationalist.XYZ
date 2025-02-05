08b9556a06052268c737f2aa8d06a5e1b690a575
Lincoln Ledet <lincolnledet@gmail.com>
2025-02-05 00:16:24 -0500

    cors

--- sensationalistbackend/index.js
+++ sensationalistbackend/index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 // 4. Serve static files from the 'uploads' directory
 app.use('/api/uploads', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Frontend origin
+  res.header('Access-Control-Allow-Origin', 'https://www.the-sensationalist.xyz'); // Frontend origin
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
